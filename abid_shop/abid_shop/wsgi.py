af372d6d4b41b4a5896fa61e49041ebdb59328f9
utf4 <utf4@gmail.com>
2022-08-19 06:08:18 +0500

    change in wsgi.py

--- abid_shop/abid_shop/wsgi.py
+++ abid_shop/abid_shop/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abid_shop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abid_shop.abid_shop.settings')
 
 application = get_wsgi_application()
